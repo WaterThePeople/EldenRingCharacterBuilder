0d013378a6530620fe187e3746e96ad6e0cd22ec
Water The People <magnificentmax0@gmail.com>
2023-11-08 15:47:52 +0100

    #update

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import style from './Main.sass';
 
 export default function Main() {
   return (
+    <ImageBackground source={require('../images/background.jpg')} resizeMode="cover" style={style.image}>
     <View style={style.container}>
-      <ImageBackground source={require('../images/background.jpg')} resizeMode="cover" style={style.image}>
-      <Text style={style.item}>Inside</Text>
-    </ImageBackground>
+      <View style={style.title}>
+          {/* <Text style={[style.title_text,{includeFontPadding: false}]}>Account name |</Text> */}
+          <Text style={{fontFamily: 'Times New Roman', color: 'white', fontSize: 40}}>Czcionka testowanie</Text>
+      </View>
     </View>
+    </ImageBackground>
   );
 }
